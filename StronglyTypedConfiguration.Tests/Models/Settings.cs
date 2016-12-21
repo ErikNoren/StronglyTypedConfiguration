edd600399dbe09e219e4ec5a03e0530e684c6747
Erik Noren <noren.erik@gmail.com>
2016-12-21 02:12:12 -0500

    Added a settings class to the test project to show centralizing settings to a single place using nested static classes to simulate namespaces and readonly members to prevent accidental assignment to shared configuration values

---
+++ StronglyTypedConfiguration.Tests/Models/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StronglyTypedConfiguration.Tests
+{
+    /// <summary>
+    /// An example settings class used to centralize all your settings.
+    /// This is an alternative to using a complex object and serializing
+    /// all your settings into a JSON string in a single app setting.
+    /// This class is static since settings can only be updated during an app pool recycle
+    /// in ASP.NET (except in Core which already has better settings support anyway).
+    /// </summary>
+    public static class Settings
+    {
+        public static readonly string AppVersion = ConfigurationManager.AppSettings
+            .GetValueOrDefault("ReleaseVersion", string.Empty);
+
+        public static class AutocompleteOptions
+        {
+            public static readonly int MaxResults = ConfigurationManager.AppSettings
+                .GetValueOrDefault("Autocomplete.MaxResults", 25);
+
+            public static readonly string NoSelectionLabel = ConfigurationManager.AppSettings
+                .GetValueOrDefault("Autocomplete.NotApplicableOptionLabel", "[none]");
+        }
+
+        public static class Administration
+        {
+            public static readonly bool SupportHasAdministrationRights = ConfigurationManager.AppSettings
+                .GetValueOrDefault("Admin.SupportHasAdminRights", false);
+        }
+    }
+}
---
+++ StronglyTypedConfiguration.Tests/SettingsClassTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StronglyTypedConfiguration.Tests
+{
+    [TestClass]
+    public class SettingsClassTests
+    {
+        [TestMethod]
+        public void SettingsClassAppVersion()
+        {
+            Assert.AreEqual("1.0.5", Settings.AppVersion);
+        }
+
+        [TestMethod]
+        public void SettingsClassAdministration()
+        {
+            Assert.AreEqual(true, Settings.Administration.SupportHasAdministrationRights);
+        }
+
+        [TestMethod]
+        public void SettingsClassAutocompleteOptions()
+        {
+            Assert.AreEqual(30, Settings.AutocompleteOptions.MaxResults);
+            Assert.AreEqual("[N/A]", Settings.AutocompleteOptions.NoSelectionLabel);
+        }
+    }
+}
